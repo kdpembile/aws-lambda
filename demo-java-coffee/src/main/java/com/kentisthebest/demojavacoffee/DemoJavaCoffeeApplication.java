771e2b5da0ea143d87b6636657ba2214274155c4
KD <kent.embile@gmail.com>
2024-02-24 23:20:05 +0800

    Initial commit

---
+++ demo-java-coffee/src/main/java/com/kentisthebest/demojavacoffee/DemoJavaCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.kentisthebest.demojavacoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaCoffeeApplication.class, args);
+	}
+
+}
---
+++ demo-java-coffee/src/test/java/com/kentisthebest/demojavacoffee/DemoJavaCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kentisthebest.demojavacoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
